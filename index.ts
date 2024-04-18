6449ca154a092941ced22ccfe404d119c5070e28
Habib-bhai <habibahmed918131@gmail.com>
2024-04-18 18:58:18 +0500

    all files uploaded

---
+++ index.ts
@@ -0,0 +1,15 @@
+#!usr/bin/env node
+//importing inquirer from "Node_modules"
+import inquirer from "inquirer"
+
+//taking user input
+const userAnswer = await inquirer.prompt({
+    name:"sentence",
+    type:"input",
+    message:"Enter your string to count its number of words."
+})
+
+const words = userAnswer.sentence.trim().split(" ")
+console.log(`Number of words in your input are:${words.length}`)
+
+
